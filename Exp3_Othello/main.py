68c90d587811cd88f2b39be3e1b95cc240c931d5
Ewan-K <2574017297@qq.com>
2021-05-05 23:06:40 +0800

    alpha-beta

---
+++ Exp3_Othello/main.py
@@ -0,0 +1,17 @@
+# 导入黑白棋文件
+from player import Player, RandomPlayer, HumanPlayer, AIPlayer
+from board import Board
+from game import Game
+import random
+
+# 人类玩家黑棋初始化
+black_player = HumanPlayer("X")
+
+# AI 玩家 白棋初始化
+white_player = AIPlayer("O", 1)
+
+# 游戏初始化，第一个玩家是黑棋，第二个玩家是白棋
+game = Game(black_player, white_player)
+
+# 开始下棋
+game.run()
